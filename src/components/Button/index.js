c1667d75e5f42f4e1e63810de4dcd1f764027a47
matheus <freitas1matheus@gmail.com>
2022-07-19 23:31:21 -0300

    Add Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+import './Button.scss'
+
+const Button = (props) => {
+    return (
+        <button className='button'>
+            {props.children}
+        </button>
+    )
+}
+
+export default Button
