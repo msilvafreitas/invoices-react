0c502b6285b08c26f392fcc0af160bdcd2c518f7
matheus <freitas1matheus@gmail.com>
2022-07-19 23:31:52 -0300

    Add Sidebar components

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return (<nav className='sidebar'>
+        <ul>
+            <li>teste</li>
+            <li>teste</li>
+            <li>teste</li>
+        </ul>
+        <p>aloiu</p>
+    </nav>)
+}
+
+export default Sidebar 
