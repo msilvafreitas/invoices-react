e1b0a717e2bccc352caf0375d31957f2f0adebd1
matheus <freitas1matheus@gmail.com>
2022-07-20 21:03:14 -0300

    Form update

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,41 @@
+import { useState } from 'react'
 import './Form.scss'
 
-const Form = () => {
+const Form = (props) => {
+
+    const [code, setCode] = useState('')
+    const [date, setDate] = useState('')
+    const [name, setName] = useState('')
+    const [value, setValue] = useState('')
+    const [status, setStatus] = useState('')
+
+    const onSave = (event) => {
+        event.preventDefault()
+        props.onInvoiceSent({
+            code,
+            date,
+            name,
+            value,
+            status
+        })
+        setCode('')
+        setDate('')
+        setName('')
+        setValue('')
+        setStatus('')
+    }
+
+
+    return (
+        <section className="form">
+            <form onSubmit={onSave}>
+                
+            </form>
+
+        </section>
+
+    )
+
 
 }
 
