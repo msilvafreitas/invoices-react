e548e9a14801edb4b53ebca0f0733a8c5f5434f3
matheus <freitas1matheus@gmail.com>
2022-07-23 21:55:38 -0300

    Invoices view
    Add new invoices to the app view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Form from './components/Form';
+import Invoice from './components/Invoice';
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
       <Header />
       <Form onInvoiceSent={invoice => onNewInvoiceAdd(invoice)}/>
       
+      {invoices.map(invoice => <Invoice key={invoice.code} code={invoice.code} name={invoice.name} date={invoice.date} value={invoice.value} status={invoice.status}/>)}
     </div>
   );
 }
